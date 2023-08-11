06a70c2b901e6a46dcd91d433ea66534192aa8c4
InHall Technologies <inhall.updatebackup@gmail.com>
2023-08-11 23:11:22 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [magicNumber, setMagicNumber] = useState({
+    name: "Ananya",
+    usn: "007",
+    college: "Hogwards",
+    home: "Hufflepuff"
+  });
+
+
+  const sampleFunction = () => {
+    setMagicNumber({
+      name: "Ananya",
+      usn: "007",
+      college: "Harvard",
+      home: "Hufflepuff"
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{magicNumber.college}</h1>
+
+      <button onClick={sampleFunction}>Say Hi!</button>
     </div>
   );
 }
